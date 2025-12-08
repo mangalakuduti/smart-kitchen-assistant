0dbdb910957158941e973093a82e125b06dab68b
Manas Angalakuduti <manasangalakuduti@gmail.com>
2025-12-07 21:48:36 -0600

    Spring Boot Initialization

---
+++ src/main/java/com/manas/smart_kitchen_assistant/SmartKitchenAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.manas.smart_kitchen_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKitchenAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKitchenAssistantApplication.class, args);
+	}
+
+}
